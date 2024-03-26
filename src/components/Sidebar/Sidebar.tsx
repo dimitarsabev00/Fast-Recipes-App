2bacdd98194d1f2cf5546c965b571c920e07e84d
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-26 05:59:47 +0200

    Added Design For SideBar Component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,43 @@
+import { ImCancelCircle } from "react-icons/im";
 import "./styles.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { setGeneralFields } from "../../store/slices/generalSlice";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const isSidebarOpen = useSelector(
+    ({ generalSlice }) => generalSlice.isSidebarOpen
+  );
+  const dispatch = useDispatch();
+  return (
+    <nav className={`sidebar ${isSidebarOpen ? "sidebar-visible" : ""}`}>
+      <button
+        type="button"
+        className="navbar-hide-btn"
+        onClick={() => {
+          dispatch(setGeneralFields({ isSidebarOpen: false }));
+        }}
+      >
+        <ImCancelCircle size={24} />
+      </button>
+
+      <div className="side-content">
+        <ul className="side-nav">
+          <li className="side-item">
+            <Link
+              to=""
+              className="side-link ls-1 fs-13"
+              onClick={() => {
+                dispatch(setGeneralFields({ isSidebarOpen: false }));
+              }}
+            >
+              Category 1
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Sidebar;
