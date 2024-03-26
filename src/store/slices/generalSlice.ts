6e54f796c18fda90251c955f328c765e48e12fc7
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-26 06:00:21 +0200

    Added isSidebarOpen Global State In Redux Store

--- src/store/slices/generalSlice.ts
+++ src/store/slices/generalSlice.ts
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  isSidebarOpen: false,
+};
 
 export const generalSlice = createSlice({
   name: "generalSlice",
