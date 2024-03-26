303d4c4025fab8c2d02b2cb3c68e201c7c7e7a63
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-26 10:26:27 +0200

    Added CategoryList Component & Render CategoryList Component In Home Screen

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Sidebar } from "./Sidebar";
 export { default as Loader } from "./Loader";
 export { default as NotFound } from "./NotFound";
+export { default as CategoryList } from "./CategoryList";
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,16 @@
 import "./styles.scss";
+import { CategoryList, Loader } from "../../components";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { categories, categoryLoading } = useSelector(
+    ({ generalSlice }) => generalSlice
+  );
+  return (
+    <main className="main-content">
+      {categoryLoading ? <Loader /> : <CategoryList categories={categories} />}
+    </main>
+  );
 };
 
 export default Home;
