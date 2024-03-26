70a137567c2217c220883b128e56edc8a6f9638d
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-26 10:16:33 +0200

    Added NotFound Component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const NotFound = () => {
+  return (
+    <div className='not-found my-5'>
+      <div className='container flex align-center justify-center'>
+        No any food found.
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Sidebar } from "./Sidebar";
 export { default as Loader } from "./Loader";
+export { default as NotFound } from "./NotFound";
